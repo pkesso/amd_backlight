6835cac28be0a1e3df18ce0613281df741cd2000
Pavel Kesso <pavel.kesso@fabrique.ai>
2023-11-20 04:51:42 +0500

    first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # amd_backlight
 backlight control script for amd based laptop
+
+## i3 config sample
+`bindcode 232 exec /usr/local/bin/backlight.sh dec 0.10`
+`bindcode 233 exec /usr/local/bin/backlight.sh inc 0.10`
---
+++ backlight.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# xrandr --output eDP --brightness $(xrandr --display :0.0 --screen 0 --verbose | awk '/Brightness/ { print $2 - 0.10; exit }')
+
+CURRENT=$(xrandr --display :0.0 --screen 0 --verbose | awk '/Brightness/ {print $2}')
+MIN=0.3
+MAX=1
+
+
+function usage {
+    echo 'Usage: backlight.sh inc|dec [step]'
+    exit 1
+}
+
+function increase {
+    NEXT_STEP=$(echo "scale=2; $CURRENT + $STEP" | bc)
+    if [ $(echo "$NEXT_STEP > $MAX" | bc -l) -eq 1 ] 
+        then
+            NEXT_STEP=$MAX
+    fi
+    xrandr --output eDP --brightness $NEXT_STEP 
+}
+
+function decrease {
+    NEXT_STEP=$(echo "scale=2; $CURRENT - $STEP" | bc)
+    if [ $(echo "$NEXT_STEP < $MIN" | bc -l) -eq 1 ] 
+        then
+            NEXT_STEP=$MIN
+    fi
+    xrandr --output eDP --brightness $NEXT_STEP 
+}
+
+if [ -z "$1" ]
+    then
+        usage
+    else
+        SIGN="$1"
+fi
+
+if [ -z "$2" ]
+    then
+        STEP=0.10
+    else
+        STEP="$2"
+fi
+
+case $SIGN in
+    inc)
+        increase
+        ;;
+    dec)
+        decrease
+        ;;
+    *)
+        usage
+        ;;
+esac
